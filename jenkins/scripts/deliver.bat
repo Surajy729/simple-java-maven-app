bcba7aab8d2f6e7d6b1c7368a8a22ea1f6522f8a
Surajy729 <Surajyadav729@gmail.com>
2026-01-29 22:28:57 +0530

    Fix delayed variable expansion in deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -3,13 +3,12 @@ setlocal enabledelayedexpansion
 
 echo ===== TROUBLESHOOTING START =====
 echo Current Directory: %cd%
-echo Listing files in target folder:
 dir target
 echo ===============================
 
 echo Extracting project name and version...
 
-:: Using Delayed Expansion (!) to ensure variables are populated immediately
+:: You MUST have the '=' after -Dexpression
 for /f "tokens=*" %%i in ('mvn -q -DforceStdout -Dmaven.test.skip^=true help:evaluate -Dexpression^=project.name') do set "NAME=%%i"
 for /f "tokens=*" %%i in ('mvn -q -DforceStdout -Dmaven.test.skip^=true help:evaluate -Dexpression^=project.version') do set "VERSION=%%i"
 
@@ -19,7 +18,7 @@ echo Verified VERSION: !VERSION!
 echo.
 
 echo The following command runs your Java application:
-:: Using ! instead of % to access the "fresh" variable value
+:: Use ! for delayed expansion to access the variables set above
 java -jar target\!NAME!-!VERSION!.jar
 
 endlocal
